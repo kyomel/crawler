860112bbc11a838b43aec160e368b7dbf89bef69
kyomel <michaellapandio04@gmail.com>
2024-09-10 16:11:51 +0700

    init repo for web crawler from bootdev

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kyomel/crawler
+
+go 1.22.5
+
+require golang.org/x/net v0.24.0 // indirect
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		return
+	}
+
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		return
+	}
+	rawBaseURL := os.Args[1]
+
+	const maxConcurrency = 3
+	cfg, err := configure(rawBaseURL, maxConcurrency)
+	if err != nil {
+		fmt.Printf("Error - configure: %v", err)
+		return
+	}
+
+	fmt.Printf("starting crawl of: %s...\n", rawBaseURL)
+
+	cfg.wg.Add(1)
+	go cfg.crawlPage(rawBaseURL)
+	cfg.wg.Wait()
+
+	for normalizeURL, count := range cfg.pages {
+		fmt.Printf("%d - %s\n", count, normalizeURL)
+	}
+}
